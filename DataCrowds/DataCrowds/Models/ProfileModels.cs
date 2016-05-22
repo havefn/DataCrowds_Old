44fab3189d56f5d84de254076543baf106af8259
aufahr <aufahr@gmail.com>
2016-05-22 20:01:03 +0700

    add all models base code

---
+++ DataCrowds/DataCrowds/Models/ProfileModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataCrowds.Models
+{
+    public class Profile
+    {
+        public DateTime birthDate { set; get; }
+        public String occupation { get; set; }
+        public String gender { get; set; }
+        
+    }
+}
---
+++ DataCrowds/DataCrowds/Models/SurveyFormModels.cs
@@ -0,0 +1,14 @@
+﻿using DataCrowds.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataCrowds.Models
+{
+    public class SurveyFormModels
+    {
+       List<Question> QuestionList { get; set; }
+
+    }
+}
