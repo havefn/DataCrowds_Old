d60dee7988051048b6ba23e223c174b1feca49c4
aufahr <aufahr@gmail.com>
2016-05-24 04:35:27 +0700

    add ActionLink for data list

--- DataCrowds/DataCrowds/Controllers/MarketplaceController.cs
+++ DataCrowds/DataCrowds/Controllers/MarketplaceController.cs
@@ -23,7 +23,7 @@ namespace DataCrowds.Controllers
         {
             System.Threading.Thread.Sleep(2000);
             var data = db.DataSets.Where(f =>
-            f.title.StartsWith(keyword)).ToList();
+            f.title.Contains(keyword)).ToList();
             return PartialView(data);
         }
 
