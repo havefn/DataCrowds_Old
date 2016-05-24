0ebff447118f2d6eac061e247e5787277f60e047
aufahr <aufahr@gmail.com>
2016-05-24 21:45:53 +0700

    add all required scripts

--- DataCrowds/DataCrowds/App_Start/BundleConfig.cs
+++ DataCrowds/DataCrowds/App_Start/BundleConfig.cs
@@ -26,6 +26,65 @@ namespace DataCrowds
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            // ====================================================================================
+            // STYLES
+            // ====================================================================================
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap")
+                            .Include("~/Content/bootstrap.css")
+                            .Include("~/Content/bootstrap-datepicker.css"));
+
+            bundles.Add(new StyleBundle("~/Content/cleditor")
+                            .Include("~/Content/jquery.cleditor.css"));
+
+            bundles.Add(new StyleBundle("~/Content/toastr")
+                            .Include("~/Content/toastr.css"));
+
+            bundles.Add(new StyleBundle("~/Content/site")
+                            .Include("~/Content/site.css"));
+
+            // ====================================================================================
+            // SCRIPTS
+            // ====================================================================================
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                            //.Include("~/Scripts/bootstrap.js")
+                            .Include("~/Scripts/bootstrap-datepicker.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                            //.Include("~/Scripts/jquery-{version}.js")
+                            .Include("~/Scripts/jquery-migrate-1.1.1.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
+                            .Include("~/Scripts/jquery.unobtrusive*")
+                            .Include("~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/cleditor")
+                            .Include("~/Scripts/jquery.cleditor.js")
+                            .Include("~/Scripts/knockout.cleditor.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/knockout")
+                            .Include("~/Scripts/knockout-{version}.js")
+                            .Include("~/Scripts/knockout.mapping-latest.js")
+                            .Include("~/Scripts/knockout.enter.js")
+                            .Include("~/Scripts/knockout.validation.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/toastr")
+                            .Include("~/Scripts/toastr.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/models")
+                            .Include("~/Scripts/vm.department.js")
+                            .Include("~/Scripts/vm.question.js")
+                            .Include("~/Scripts/vm.responselist.js")
+                            .Include("~/Scripts/vm.survey.js")
+                            .Include("~/Scripts/vm.surveylist.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when 
+            // you're ready for production, use the build tool at http://modernizr.com to pick 
+            // only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr")
+                            .Include("~/Scripts/modernizr-*"));
         }
     }
 }
