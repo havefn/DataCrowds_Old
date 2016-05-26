e8bb057837eff56a282a466c82dff97ae326231c
aufahr <aufahr@gmail.com>
2016-05-27 06:19:00 +0700

    fix carousel

--- DataCrowds/DataCrowds/Migrations/Configuration.cs
+++ DataCrowds/DataCrowds/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace DataCrowds.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(DataCrowds.Models.ApplicationDbContext context)
